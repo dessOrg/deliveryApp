6725721bfa84623b605c482ab70fd686871605c9
dessHub <deskip95@gmail.com>
2016-08-29 16:55:10 +0300

    add rows and panels to home.html page

---
+++ public/js/jquery/slide.js
@@ -0,0 +1,8 @@
+<script src="jquery.min.js"></script>
+<script src="jssor.slider.mini.js"></script>
+<script>
+    jQuery(document).ready(function ($) {
+        var options = { $AutoPlay: true };
+        var jssor_slider1 = new $JssorSlider$('slider1_container', options);
+    });
+</script>
